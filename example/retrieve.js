ee5b6131748fafee3a252c791e630bb8498b3495
azukiwasher <nobuhiro.uchiyama@higanworks.com>
2011-10-27 10:53:19 +0900

    update examples

--- example/retrieve.js
+++ example/retrieve.js
@@ -8,11 +8,13 @@ var giraffi = require('../lib/giraffi');
 
 // Define a applog server
 var config = {
-  host: "localhost:3000"
+  host: "localhost:3000",
+  ssl: false
 };
 
 // Create a client object and start retrieving logs
 var client = giraffi.createClient(config);
+
 client.level('info').limit(3).retrieve('foo', function (err, results) {
   console.log("results: ", results);
 });
